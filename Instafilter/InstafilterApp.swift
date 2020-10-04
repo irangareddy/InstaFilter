2f6244222a381a475502df5d48d1c84ab9170574
irangareddy <itsmerr17@gmail.com>
2020-10-04 18:56:38 +0530

    Part 1&2

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by RANGA REDDY NUKALA on 02/10/20.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
